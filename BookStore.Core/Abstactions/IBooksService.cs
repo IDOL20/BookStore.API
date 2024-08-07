2a6f814373f0ea0aaefad13929d4d0219a5da138
Andrej Kallaur <kallaur00xxx1@mail.ru>
2024-08-07 06:17:25 +0300

    Add project files.

---
+++ BookStore.Core/Abstactions/IBooksService.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Core.Models;
+
+namespace BookStore.Application.Services
+{
+    public interface IBooksService
+    {
+        Task<Guid> CreateBook(Book book);
+        Task<Guid> DeleteBook(Guid id);
+        Task<List<Book>> GetAllBooks();
+        Task<Guid> UpdateBook(Guid id, string title, string description, decimal price);
+        Task UpdateBook(Guid id, object title, object description, object price);
+    }
+}
---
+++ BookStore.DataAccess/BookStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.DataAccess.Entites;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BookStore.DataAccess
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookEntity> Books { get; set; }
+    }
+}
